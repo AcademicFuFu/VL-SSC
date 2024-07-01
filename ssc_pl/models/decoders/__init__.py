5144af089f5a70c61c21ca57f6ce8777fd8e6001
AcademicFuFu <samuraiwry@gmail.com>
2024-07-01 17:09:15 +0800

    [Feat]: implement decoder v2

--- ssc_pl/models/decoders/__init__.py
+++ ssc_pl/models/decoders/__init__.py
@@ -1,4 +1,5 @@
 from .symphonies_decoder import SymphoniesDecoder
 from .vl_decoder_v0 import VisionLanguageDecoderV0
 from .vl_decoder_v1 import VisionLanguageDecoderV1
+from .vl_decoder_v2 import VisionLanguageDecoderV2
 from .unet3d import UNet3D
